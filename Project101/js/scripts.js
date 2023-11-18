36e83eb45aa030a1a40fab07b4d0412b8af7d956
deviousluck <91898213+deviousluck@users.noreply.github.com>
2023-11-18 19:37:42 +0300

    Add files via upload

---
+++ Project101/js/scripts.js
@@ -0,0 +1,9 @@
+// Highlights current date on contact page
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
+
+/*
+In summary, this JavaScript code is designed to highlight the current day of the week on a web page, specifically on the contact page. It uses the "DOMContentLoaded" event to ensure that the code runs after the HTML document has loaded. The code selects the list items containing information about the hours of operation and adds a "today" class to the list item corresponding to the current day of the week. This allows visitors to quickly identify the current day's operating hours on the contact page.
+*/
